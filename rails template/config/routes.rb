fa5aad3d0b4af7e9ee388655102566299d996b0d
dbohorquez8 <dbohorquez8@gmail.com>
2012-11-13 09:39:44 -0500

    updated rails haml template with new grid system

--- rails template/config/routes.rb
+++
@@ -1,8 +0,0 @@
-WwmMarkupRails::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  root to: "pages#index"
-  
-  get "/:action", controller: "pages"
-end
