c0ea825ed6324d4ea5b3e616cad2797702d6d3dc
Mauricio <mauricio.mejia@koombea.com>
2013-05-27 17:43:57 -0500

    added new mobiel web app erb template

---
+++ mobile_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwmMarkupRails::Application.config.secret_token = '1380d8ec7ec24b5d389311e46dcdb6970aa8f5e708e6f86a37ec7c11f9c8c96e5411db455974518d6369228420b1a255dac6be05ae08bd42bbe904f699a7d465'
